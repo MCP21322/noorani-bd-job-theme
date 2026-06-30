6d5b5df41096b9b23df1f365db3e44119478e413
Mahady Hasan <your_email@example.com>
2026-06-30 22:45:54 +0600

    widget function change

--- templet_part/widget_function.php
+++ templet_part/widget_function.php
@@ -1,4 +1,4 @@
-<p>This is site ber area</p>
+
 <?php if(is_active_sidebar('main_sidebar_1')): ?>
     <div id="secondary " class='p-3 bg-light border rounded'>
         <?php dynamic_sidebar('main_sidebar_1');  ?>
